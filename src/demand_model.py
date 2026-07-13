d265bb96b9fbd7087d9de8e94815bb667d508e00
willowkatze <weiyehu2003@gmail.com>
2026-07-13 15:33:51 +0200

    Add demand model placeholder

---
+++ src/demand_model.py
@@ -0,0 +1,5 @@
+"""Demand-model estimation placeholders.
+
+This module will contain demand estimation logic once exploratory analysis has
+identified suitable variables and modeling assumptions.
+"""
